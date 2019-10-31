3f0b4143793ad92f2a98ea6de22da4d7cefa47da
Zvonimir Dragun <zed.dragun@gmail.com>
2019-10-31 13:17:48 +0700

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,11 +20,19 @@ export default props => (
           <b className="text-dark">Contact Us</b>
         </Nav.Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1"><b className="text-dark">Link</b></NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2"><b className="text-dark">Another action</b></NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3"><b className="text-dark">Something</b>/NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">
+            <b className="text-dark">Link</b>
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">
+            <b className="text-dark">Another action</b>
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">
+            <b className="text-dark">Something</b>/NavDropdown.Item>
+          </NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4"><b className="text-dark">Separated link</b></NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">
+            <b className="text-dark">Separated link</b>
+          </NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
